0a7bfd6d02752ddca2e2d6a146e18ecdd1e183fc
jian.chen@sohan.hk <jian.chen@sohan.hk>
2020-05-22 15:28:19 +0800

    web

---
+++ 01.web/src/test/java/chen/example/security/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package chen.example.security.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
